48eae41dc4ecc45babd75e88fdb5aa47c614bbcf
FranciscoMateusVG <49985463+FranciscoMateusVG@users.noreply.github.com>
2024-05-20 09:14:02 -0300

    week 1 exercises

---
+++ week 1/5-string_reversal.py
@@ -0,0 +1,5 @@
+# Write a Python program that takes a string input from the user and prints the string in reverse.
+
+# Hint: You can use string slicing to reverse a string. 
+string = input("Enter a string: ")
+
---
+++ week 1/6-multiplication_table copy.py
@@ -0,0 +1,11 @@
+# Write a Python program that prints the multiplication table for a given number.
+# The user should input the number, and the program should print the table from 1 to 10.
+
+# Hint: The output should look like this if the user enters 5:
+# 5 x 1 = 5
+# 5 x 2 = 10
+# 5 x 3 = 15
+# 5 x 4 = 20
+# ...
+
+number = int(input("Enter a number: "))
