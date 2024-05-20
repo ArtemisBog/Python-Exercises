350969060f40a9c0f7563c89e10a76372a1a7775
Artem <110948830+ArtemisBog@users.noreply.github.com>
2024-05-20 10:29:44 -0400

    Excersize 1 finished

--- week 1/1-basic_arithmetic.py
+++ week 1/1-basic_arithmetic.py
@@ -5,3 +5,13 @@
 # Hint: You can use the input() function to get user input. For example:
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
+
+sum = num1 + num2
+difference = num1 - num2
+product = num1*num2
+quotient = num1/num2
+
+print("The sun of the two numbers is ", sum) 
+print("The difference of the two numbers is ", difference)
+print("The product of the two numbers is ", product)
+print("The quotient of the two numbers is ", quotient)
