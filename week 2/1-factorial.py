3c8584e1e99be2847f56af607ab820abeaccdc71
Artem <110948830+ArtemisBog@users.noreply.github.com>
2024-05-27 22:13:12 -0400

    Week 2 task 1 completed

--- week 2/1-factorial.py
+++ week 2/1-factorial.py
@@ -1,3 +1,15 @@
 # Write a function to calculate the factorial of a given number.
 # Factorial is the product of an integer and all the integers below it;
 # Example: The factorial of 4 is 4*3*2*1 = 24
+
+
+number = int(input("Enter the number: "))
+
+def factorial(number):
+    f = 1
+    for i in range(1, number + 1):
+        f *= i
+    return f
+
+# to_print = factorial(number) 
+print(factorial(number))
